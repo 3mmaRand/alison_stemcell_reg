6729b575f2fd148266aba6b476d7a8f82dd5d987
Emma Rand <emma.rand@york.ac.uk>
2020-05-17 16:48:07 +0100

    HDI and JIF added to dataset

--- scripts/working.R
+++ scripts/working.R
@@ -1,96 +1,40 @@
-stages <- tibble(
-  clicks = 1,
-  signups = .47,
-  cart = .09,
-  buys = .05
-) %>%
-  gather(stage, percent)
-
-stages
-to_poly <- stages %>%
-  arrange(percent) %>%
-  mutate(
-    group = row_number(),
-    x1 = 0 + ((1 - percent)/2),
-    x4 = percent + ((1 - percent)/2),
-    x2 = lag(x1), 
-    x3 = lag(x4)
-  ) %>%
-  mutate(
-    x2 = ifelse(is.na(x2), x1, x2),
-    x3 = ifelse(is.na(x3), x4, x3)
-  ) %>%
-  gather(key = xpoint, value = x, -stage,-group, -percent) %>%
-  arrange(group, xpoint) %>%
-  mutate(y = ifelse(xpoint %in% c("x2", "x3"), group, group + 1))
-
-to_poly
-
-#########################
-orig <- data.frame(step_names = c("mesenchymal",
-                                  "article",
-                                  "cell tissue etc",
-                                  "clinical etc",
-                                  "not biotech etc"),
-                   n = c(65706,
-                         47031,
-                         7814,
-                         5502,
-                         2055))
-
-orig$step_names <- fct_relevel(orig$step_names,
-                               "mesenchymal",
-                               "article",
-                               "cell tissue etc",
-                               "clinical etc",
-                               "not biotech etc")
-# prep polygons that connect
-to_poly_orig <- orig %>%
-  arrange(n) %>%
-  mutate(
-    group = row_number(),
-    x1 = 0 + ((1 - n)/2),
-    x4 = n + ((1 - n)/2),
-    x2 = lag(x1), 
-    x3 = lag(x4)) %>%
-  mutate(
-    x2 = ifelse(is.na(x2), x1, x2),
-    x3 = ifelse(is.na(x3), x4, x3)
-  ) %>%
-  gather(xpoint, x, -step_names,-group, -n) %>%
-  arrange(group, xpoint) %>%
-  mutate(y = ifelse(xpoint %in% c("x2", "x3"), group, group + 1))
+library(tidyverse)
+library(janitor)
 
 
-# EXAMPLE
-# Create the placement of the stages in the plot,
-#  by just adding a 0.5 to each row number
+file <- "data/checked_processed_aw_corrections.csv"
+dat <- read_csv(file, skip = 1) 
 
-labels <- orig %>%
-  arrange(n) %>%
-  mutate(y = row_number() + 0.5)
+# note: 2019 values were made equal to 2018 values
+file <- "data/Human development index (HDI).csv"
+hdi <- read_csv(file) %>% clean_names() %>% select(-hdi_rank_2018)
 
-cbPalette <- brewer.pal(6,"Blues")
+hdi <- hdi %>% 
+  pivot_longer(names_to = "year",
+               values_to = "index",
+               -country) %>% 
+  mutate(year = as.numeric(str_remove(year, "x"))) %>% 
+  filter(year > 2009) %>% 
+  filter(country %in% dat$country)
 
-to_poly_orig %>%
-  ggplot() +
-  geom_polygon(aes(x,
-                   y,
-                   group = group,
-                   fill = step_names)) +
-  geom_text(aes(x = 0.5,
-                y = y,
-                label = n),
-            data = labels, size = 3) +
-  scale_y_continuous(breaks = labels$y,
-                     labels = labels$step_names) +
-  theme_minimal() +
-  theme(
-    legend.position = "none", 
-    axis.title = element_blank(), 
-    axis.text.x = element_blank(), 
-    axis.ticks.x = element_blank(), 
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank(), 
-    panel.grid.minor.y = element_blank()) +
-  scale_fill_manual(values = cbPalette)
+
+length(unique(dat$country))
+length(unique(hdi$country))
+
+test <- merge(dat, hdi, by = c("country", "year"), all.x = TRUE)
+
+test[is.na(test$index), c("country","year")]
+
+file <- "data/test.csv"
+write_csv(test, file)
+
+
+data_dir <- "data/jif/"
+jif_files <- fs::dir_ls(data_dir)
+
+
+jif <- jif_files %>% 
+  map_dfr(read_csv)
+names(jif)[2] <- "journal"
+
+test <- merge(dat, jif, by = c("journal", "year"), all.x = TRUE)
